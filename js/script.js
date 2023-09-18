0a80b1a70e1bc9ae41622e7925975b8b12cc3348
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2023-09-18 08:07:41 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 mensagem = (msg) => alert(msg);
 soma = (a,b) => a + b;
-
+sub = (a,b) => a - b;
+mult = (a,b) => a * b;
+div = (a,b) => a / b;
+raiz = x => Math.sqrt(x);
+// mensagem(soma(10,soma(8,2)));
+// mensagem(soma(2,div(2,2)));
+//mensagem(soma(div(80,2),mult(5,3)));
+//mensagem(soma(sub(8,5),sub(3,70)));
+/* Não copiem a parte verde.
+    Qual o resultado da soma das expressões:
+    a) 10 + 8 + 2
+    b)  2 + 2 / 2
+    c) 80/2 + 5 * 3
+    d) 8 - 5 + 3 - 70
+*/
 
