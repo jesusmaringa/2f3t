1d032907157b6247b76739dff7b414fa91307a57
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2023-10-02 08:26:31 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,15 +4,44 @@ sub = (a,b) => a - b;
 mult = (a,b) => a * b;
 div = (a,b) => a / b;
 raiz = x => Math.sqrt(x);
-// mensagem(soma(10,soma(8,2)));
-// mensagem(soma(2,div(2,2)));
-//mensagem(soma(div(80,2),mult(5,3)));
-//mensagem(soma(sub(8,5),sub(3,70)));
-/* Não copiem a parte verde.
-    Qual o resultado da soma das expressões:
-    a) 10 + 8 + 2
-    b)  2 + 2 / 2
-    c) 80/2 + 5 * 3
-    d) 8 - 5 + 3 - 70
-*/
 
+let a = "";
+let b = "";
+let valor = "";
+let temPonto = false;
+
+function escrever_display(num){
+    document.getElementById("resultado").value=num;
+}
+function digitando(tecla){
+   if( tecla == "."){
+        if(!temPonto){
+            temPonto = true;
+            valor += tecla;
+            escrever_display(valor);
+        }
+        return;
+   }
+   valor += tecla;
+   escrever_display(valor);
+}
+let executa = "";
+function operacao(op){
+    executa = op;
+    a = valor;
+    valor = "";
+    temPonto = false;
+}
+function calcula(){
+    if(executa != ""){
+        b=valor;
+        if(executa == "mult") escrever_display(mult(a,b));
+        if(executa == "div") escrever_display(div(a,b));
+        if(executa == "sub") escrever_display(sub(a,b));
+        if(executa == "soma") escrever_display(soma(Number(a),Number(b)));
+        a = "";
+        b = "";
+        valor = "";
+        temPonto = false;
+    }
+}
